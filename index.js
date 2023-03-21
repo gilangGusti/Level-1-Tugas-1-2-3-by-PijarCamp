b1fa6793887f7dca01168d69b0eff2086801c787
Gemilang Karunia Gusti <125884682+gilangGusti@users.noreply.github.com>
2023-03-21 23:09:02 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,125 @@
+//Nama
+const nama = "Gemilang Karunia Gusti";
+console.log(nama);
+//Umur
+const age = "22";
+console.log(age);
+//Alamat
+const address = {
+  kabupaten: "Bogor, Dramaga",
+};
+console.log(address);
+//Hobi
+const hobi = ["Musik, Futsal, Lari, Bola Volli, Renang"];
+console.log(hobi);
+//Universitas
+const Universitas = "Universitas Binaniaga";
+console.log(Universitas);
+//Kenapa Tertarik Programing
+const alasan = "Ingin Memperdalam skill yang telah dipelajari di fase kuliah";
+console.log(alasan);
+//Hal yang sudah dipelajari di Programming
+const sebab = "Php, html, dan CSS";
+console.log(sebab);
+//pernah kesulitan ketika belajar programming
+const kesulitan = "Sering Kesulitan";
+console.log(kesulitan);
+//alasan kenapa ingin ikut bootcamp pijar camp
+const ingin_ikut = "Ingin Mempelajari banyak skill yang ada di programing";
+console.log(ingin_ikut);
+//alasan kenapa kami harus terima kalian di Bootcamp Pijar Camp
+const kenapa =
+  "Saya memiliki semangat belajar yang tinggi, gemar mempelajari hal baru untuk meningkatkan skill yang saya miliki, dan saya adalah seorang yang disiplin dan mampu berkomunikasi dengan sangat baik";
+console.log(kenapa);
+console.log("-------")
+//No.1
+console.log("No. 1 Tugas 2");
+var name = "Gemilang Karunia Gusti";
+console.log(name);
+let umur = "22";
+console.log(umur);
+const alamat = {
+  Provinsi: "Jawa Barat",
+  Kabupaten: "Bogor",
+  Kecamatan: "Dramaga",
+  Kelurahan: "Dramaga",
+};
+console.log(alamat);
+var hoby = ["Musik, Futsal, Lari, Bola Volli, Renang"];
+console.log(hoby);
+const asal_univ = "Universitas Binaniaga";
+console.log(asal_univ);
+const why = "Ingin Memperdalam skill yang telah dipelajari di fase kuliah";
+console.log(why);
+console.log("----");
+
+//no.2
+console.log("NO. 2 Tugas 2");
+var a = "Gemilang Karunia Gusti ";
+var b = "Universitas Binaniaga";
+var c = "Sistem Informasi";
+var d =
+  "Karena dengan programming juga bisa membuat saya fokus terhadap suatu hal, dan saya suka dengan dunia programming";
+console.log(
+  "Nama saya adalah : " +
+  a +
+  "                           Asal Universitas : " +
+  b +
+  "                             Jurusan Saya adalah : " +
+  c +
+  "                               Alasan Saya tertarik dengan programming adalah : " +
+  d
+);
+console.log("------");
+//No.3
+console.log("No.3 Tugas 2");
+var v =
+  " var adalah function scope yang dimana variabel var dapat diperbarui dan dideklarasikan ulang didalam  scopenya ";
+var l =
+  " sedangkan let adalah scope blok yang dimana variabel yang dapat diperbarui namun tidak dapat dideklarasikan ulang";
+var c =
+  " sama seperti hal nya let, const adalah scope blok tapi variabel const tidak dapat diperbarui maupun dideklarasikan ulang";
+console.log("Perbedaan var, let dan const adalah " + v + l + c);
+      //no.1 Tugas 3
+      // comments Baris Pertama
+      /** Tipe Data Di JavaScript Adalah
+       * Number.
+       * BigInt.
+       * String.
+       * Boolean.
+       * Null.
+       * Undefined.
+       * Symbol.
+       */
+    
+    // no. 2 Tugas 3
+    //Tipe Data String di JavaScript
+    const namaDepan =" Gemilang Karunia";
+    const namaBelakang ="Karunia";
+    let addres = "Bogor"
+    const asal_Univ = "Universitas Binaniaga";
+    const jenis_kelamin = "Laki-laki";
+    var hobby = ["Musik, Futsal, Lari, Bola Volli, Renang"];
+
+    //Tipe Data Numbers di JavaScript
+    let umur_saya ="22";
+    let tgl_lahir ="17 April 2000";
+    let no_hp = "08515728182";
+    let no_mahasiswa = "1871273";
+    // no.3 tugas 3 
+    console.log("-----")
+    console.log("No.3 Tugas 3")
+    const AngkaPertama = 3456;
+    const AngkaKedua = 1234;
+    //Penjumlahan
+    total = AngkaPertama + AngkaKedua;
+    console.log (total);
+    //Pengurangan
+    total = AngkaPertama - AngkaKedua;
+    console.log (total)
+    //Perkalian
+    total = AngkaPertama * AngkaKedua;
+    console.log(total)
+    //Pembagian
+    total = AngkaPertama / AngkaKedua;
+    console.log(total)
